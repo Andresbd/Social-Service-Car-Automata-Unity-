1c55fd36a91f66b42443ed3a38b0b9ff51195e54
Andrés Bustamante <andres.bd@outlook.com>
2018-09-17 19:02:28 -0500

    First Commit

---
+++ Assets/Scripts/cargarEscena.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class cargarEscena : MonoBehaviour {
+
+	// Use this for initialization
+	public void loadScene(int scene){
+		SceneManager.LoadScene (scene);
+	}
+
+}
